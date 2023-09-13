1c7e62137eb7555f65295224ca4d21161ecb9abc
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2023-09-13 23:14:32 +0300

    [layout] Add Navbar component for better navigation.

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import {  DM_Sans } from "next/font/google";
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={dm_sans.className}>
+        <Navbar/>
         {children}
       </body>
     </html>
