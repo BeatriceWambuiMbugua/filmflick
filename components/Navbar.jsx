e28293a5377055109dccfdc46d668fb4998a7616
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2023-09-14 11:57:19 +0300

    Add mobile menu toggle button and mobile menu items in the Navbar component. This is necessary to provide a responsive design and improve mobile user experience.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,20 @@
-import Image from "next/image";
-import React from "react";
+"use client"
 import { MdOutlineOndemandVideo, MdOutlineSearch } from "react-icons/md";
 import { HiMenuAlt4 } from "react-icons/hi";
+import {BiMenu} from "react-icons/bi"
+import {AiOutlineClose} from "react-icons/ai"
+
+import { useState } from "react";
+
 
 export default function () {
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+      setMobileMenuOpen(!isMobileMenuOpen);
+    };
   return (
-    <div className="flex justify-around items-center py-10">
+    <div className="flex justify-around items-center py-10 bg-black">
       <div className="flex gap-8">
         <div className="w-10 h-10 bg-[#BE123C] text-white rounded-full flex items-center justify-center">
           <MdOutlineOndemandVideo size={20} />
@@ -15,7 +24,28 @@ export default function () {
         </h3>
       </div>
 
-      <div class="relative w-[526px]">
+        {/* Mobile Menu Toggle Button */}
+        {/* <div className="md:hidden">
+          <button
+            className="text-white focus:outline-none"
+            onClick={toggleMobileMenu}
+          >
+            {isMobileMenuOpen ? (
+              <AiOutlineClose className="w-6 h-6" />
+            ) : (
+              <BiMenu className="w-6 h-6" />
+            )}
+          </button>
+        </div>
+
+        <div
+          className={`${
+            isMobileMenuOpen ? 'block' : 'hidden'
+          } md:flex md:space-x-8 items-center`}
+        >     
+        </div> */}
+
+      <div class="relative w-[526px] hidden md:flex">
         <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
           <MdOutlineSearch className="w-6 h-6 text-gray-500" />
         </div>
@@ -27,7 +57,7 @@ export default function () {
           required
         />
       </div>
-      <div className="flex gap-8">
+      <div className="hidden gap-8 md:flex">
         <h3 className="text-white text-[24px] font-bold break-words">
           Sign in
         </h3>
