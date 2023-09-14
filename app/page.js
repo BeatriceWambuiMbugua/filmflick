f5d233746a35bb3922591ac7dfb79e7f2b024b56
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2023-09-14 11:56:41 +0300

    [app/page.js] Refactor Home component to fetch top 10 movies and display using Results component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,30 @@
+"use client"
+import Card from "@/components/Card";
+import { fetchTop10Movies } from "./api/items/route";
+import Results from "@/components/Results";
+import React, { useEffect, useState } from "react";
 
 
-export default function Home() {
+
+export default async function Home() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await fetchTop10Movies();
+        setResults(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-  <>
-  <h2>Featured movie</h2>
-  <p>John wick</p>
-  </>
-  )
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
